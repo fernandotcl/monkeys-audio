56de777ed5524f51b3a5051fc8cdcc961ea1cd2d
Fernando Lemos <fernandotcl@gmail.com>
2018-03-11 15:27:15 +0100

    Remove the definitions of min/max

--- src/Shared/NoWindows.h
+++ src/Shared/NoWindows.h
@@ -39,8 +39,6 @@ typedef unsigned char       UCHAR;
 typedef const wchar_t *     LPCWSTR;
 
 #define ZeroMemory(POINTER, BYTES) memset(POINTER, 0, BYTES);
-#define max(a,b)    (((a) > (b)) ? (a) : (b))
-#define min(a,b)    (((a) < (b)) ? (a) : (b))
 
 #define __stdcall
 #define CALLBACK
